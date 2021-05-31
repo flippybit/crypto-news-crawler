2903b760cc7d8bd9bfc5ed26e92ccf7db797fd8d
benja <ruizbr2020@gmail.com>
2021-06-01 00:34:09 +0200

    spider funcion created

--- crawl.py
+++ crawl.py
@@ -1,5 +1,33 @@
 import requests
-from bs4 import beautifulsoup4
+import asyncio
+import aiohttp
+from bs4 import BeautifulSoup as bs
 
-resp = requests.get("www.coindesk.com")
-print(resp.text)
+resp = requests.get("http://www.coindesk.com/tag/bitcoin-mining")
+
+soup = bs(resp.text)
+print(soup.prettify())
+link_list = []
+soup = bs(resp.text, "html.parser")
+print("The href links are :")
+for link in soup.find_all('a'):
+   print(link.get('href'))
+   link_list.append(link.get('href'))
+
+
+print("printing filterd links********************************************")
+for i ,link in enumerate (link_list):
+
+    if ("crypto" in str(link)):
+        print (i,link)
+
+
+async def spider():
+    async with aiohttp.ClientSession() as session:
+         async with session.get('http://httpbin.org/get') as resp:
+                print(resp.status)
+                print(await resp.text())
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(spider())
